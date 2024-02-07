a26dcd800e81049f390712ac614f3b9318edbad3
Omkar Chandorkar <gotenksIN@aospa.co>
2024-02-07 14:21:48 +0000

    porsche: overlay: Configure aux camera for Aperture Change-Id: I14e9bf9ba71c9d9f4aca04f3b558a1d72e248a60
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: COSMIC <rayavanindra@gmail.com>
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- device.mk
+++ device.mk
@@ -61,6 +61,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-voltage
 
 PRODUCT_PACKAGES += \
+    ApertureOverlay \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
